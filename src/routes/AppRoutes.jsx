922f3643b9b814c697cfd04404feeade5d13179a
talha9847 <cse.210840131054@gmail.com>
2026-09-23 16:34:16 +0530

    app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -35,6 +35,21 @@ import BDClient from "../pages/bd/Client";
 import BDCreateClient from "../pages/bd/CreateClient";
 import BDClientDetails from "../pages/bd/ClientDetails";
 
+import { PROJECT_LEAD_NAV_SECTIONS } from "./projectLeadRouteConfig";
+
+import ProjectLeadDashboard from "../pages/projectLead/Dashboard";
+import AssignedProjects from "../pages/projectLead/AssignedProjects";
+import ProjectPlanning from "../pages/projectLead/ProjectPlanning";
+import Team from "../pages/projectLead/Team";
+import TechnologyStack from "../pages/projectLead/TechnologyStack";
+import ERDiagram from "../pages/projectLead/ERDiagram";
+import Flowchart from "../pages/projectLead/Flowchart";
+import Phases from "../pages/projectLead/Phases";
+import ModulesFeatures from "../pages/projectLead/ModulesFeatures";
+import Tasks from "../pages/projectLead/Tasks";
+import KanbanBoard from "../pages/projectLead/KanbanBoard";
+import ProjectProgress from "../pages/projectLead/ProjectProgress";
+
 import {
   ADMIN_NAV_SECTIONS,
   HR_NAV_SECTIONS,
@@ -133,7 +148,33 @@ function AppRoutes() {
           </Route>
         </Route>
       </Route>
-
+      <Route element={<RoleRoute allowedRoles={[ROLES.PROJECT_LEAD]} />}>
+        <Route
+          element={<DashboardLayout sections={PROJECT_LEAD_NAV_SECTIONS} />}
+        >
+          <Route
+            path="/project-lead/dashboard"
+            element={<ProjectLeadDashboard />}
+          />
+          <Route path="/project-lead/projects" element={<AssignedProjects />} />
+          <Route path="/project-lead/planning" element={<ProjectPlanning />} />
+          <Route path="/project-lead/team" element={<Team />} />
+          <Route
+            path="/project-lead/technology-stack"
+            element={<TechnologyStack />}
+          />
+          <Route path="/project-lead/er-diagram" element={<ERDiagram />} />
+          <Route path="/project-lead/flowchart" element={<Flowchart />} />
+          <Route path="/project-lead/phases" element={<Phases />} />
+          <Route
+            path="/project-lead/modules-features"
+            element={<ModulesFeatures />}
+          />
+          <Route path="/project-lead/tasks" element={<Tasks />} />
+          <Route path="/project-lead/kanban" element={<KanbanBoard />} />
+          <Route path="/project-lead/progress" element={<ProjectProgress />} />
+        </Route>
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
